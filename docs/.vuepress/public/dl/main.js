84e242caa6d23156a1261af759cad81b3def423f
MrXiaoM <coolxiaom95@gmail.com>
2025-08-17 19:50:11 +0800

    Update main.js

--- docs/.vuepress/public/dl/main.js
+++ docs/.vuepress/public/dl/main.js
@@ -63,9 +63,9 @@
 			}
 			$downloadTable.append(
 				'<tr><td colspan="2"><ul class="inline-ul">' +
-				  '<li><a target="_blank" href="https://github.com/' + author + '/' + name + '">Github</a></li>' +
-				  '<li><a target="_blank" href="https://github.com/' + author + '/' + name + '/releases">Releases</a></li>' +
-				  '<li><a target="_blank" href="https://nightly.link/' + author + '/' + name + '/workflows/build/main/' + name + '">最新开发版本下载</a></li>' +
+				  '<li><a class="fab fa-github" target="_blank" href="https://github.com/' + author + '/' + name + '"> Github</a></li>' +
+				  '<li><a class="far fa-folder-open" target="_blank" href="https://github.com/' + author + '/' + name + '/releases"> Releases</a></li>' +
+				  '<li><a class="far fa-code" target="_blank" href="https://plugins.mcio.dev/actions?author=' + author + '&repo=' + name + '"> 下载开发版本</a></li>' +
 				'</ul></td></tr>');
 			for (let i = 0; i < json.length; i++) {
 				let obj = json[i];
