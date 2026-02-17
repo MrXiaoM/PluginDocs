3e25055b92e447f0f86d90f11b7f1b659e96812b
MrXiaoM <coolxiaom95@gmail.com>
2026-02-17 22:17:50 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -31,7 +31,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 导航配置
     nav: [
       { text: '首页', link: '/' },
-      { text: '赞助', link: '/feedme' },
+      { text: '赞助', link: '/feedme/' },
       { text: '无法下载?', link: 'https://plugins.mcio.dev/dl' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
