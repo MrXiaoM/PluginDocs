807a970092b5d6731012ab1dfc6b6f7302cfa818
MrXiaoM <coolxiaom95@gmail.com>
2026-06-02 08:48:19 +0800

    remove unnecessary vuepress plugins

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -157,16 +157,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     'tabs',
     
     [
-      'one-click-copy', // 代码块复制按钮
-      {
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-        duration: 1000, // prompt message display time.
-        showInMobile: false, // whether to display on the mobile side, default: false.
-      },
-    ],
-
-    [
       'vuepress-plugin-zooming', // 放大图片
       {
         selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
