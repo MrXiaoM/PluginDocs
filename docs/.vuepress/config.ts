80ffdad8d447cd71bfc3197c48fb499aa9da4a48
MrXiaoM <coolxiaom95@gmail.com>
2026-08-22 20:59:49 +0800

    add contact group page

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -31,6 +31,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     nav: [
       { text: '首页', link: '/' },
       { text: '赞助', link: '/feedme/' },
+      { text: '交流群', link: '/contact/' },
       { text: '无法下载?', link: 'https://plugins.mcio.dev/dl' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
